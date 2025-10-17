ebfe37f97e87c87427a18a1e92d47e0a8256b5fb
Michaeldlt1 <mbm.torre@yavirac.edu.ec>
2025-10-17 09:10:42 -0500

    database

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -1,18 +1,21 @@
 // database.provider.ts
+import { Inject } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { DataSource } from 'typeorm';
 
 export const databaseProviders = [
   // <- este nombre debe coincidir
   {
     provide: 'DATABASE_CONNECTION',
-    useFactory: () => {
+    Inject: [ConfigService],
+    useFactory: (config: ConfigService) => {
       const dataSource = new DataSource({
         type: 'postgres',
-        host: 'localhost', 
-        port: 5432,
-        username: 'postgres',
-        password: '12345',
-        database: 'backen_alex_zaruma',
+        host: config.get('HOST'),
+        port: config.get('PORT_DB'),
+        username: config.get('USERNAME'),
+        password: config.get('PASSWORD'),
+        database: config.get('DATABASE'),
       });
       return dataSource.initialize();
     },
